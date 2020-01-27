83714e0391fd88d1d321ef926d1ec69a87615ac2
Michael Messmer <mmessmer@mail.csuchico.edu>
2020-01-27 11:37:41 -0800

    permission check added

--- modbot.py
+++ modbot.py
@@ -4,15 +4,16 @@ from dotenv import load_dotenv
 from discord.ext import commands
 
 load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+token = os.getenv("DISCORD_TOKEN")
 
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+    print(f"{bot.user.name} has connected to Discord!")
 
 @bot.command()
+@commands.has_any_role("Mod", "It Me")
 async def hello(ctx):
     await ctx.send("Hello World!")
 
