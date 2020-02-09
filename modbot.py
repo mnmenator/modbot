64934259dcce01bee44c3e44d0bc3dcfed345f6b
Michael Messmer <mmessmer@mail.csuchico.edu>
2020-02-08 19:18:12 -0800

    restrict commands to bot-cli channel

--- modbot.py
+++ modbot.py
@@ -8,11 +8,20 @@ token = os.getenv("DISCORD_TOKEN")
 
 bot = commands.Bot(command_prefix='!')
 
+def bot_cli():
+    async def predicate(ctx):
+        if ctx.message.channel.name != "bot-cli":
+            await ctx.message.delete()
+            raise commands.DisabledCommand()
+        return True
+    return commands.check(predicate)
+
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} has connected to Discord!")
 
 @bot.command()
+@bot_cli()
 @commands.has_role("Admin")
 async def hello(ctx):
     await ctx.send("Hello World!")
@@ -23,5 +32,7 @@ async def hello_error(ctx, error):
         print("Someone other than an admin attempted this command!")
     elif isinstance(error, commands.NoPrivateMessage):
         print("Someone tried to execute this command in a private message!")
+    elif isinstance(error, commands.DisabledCommand):
+        print("Someone tried to execute this command outside of the cli channel!")
 
 bot.run(token)
